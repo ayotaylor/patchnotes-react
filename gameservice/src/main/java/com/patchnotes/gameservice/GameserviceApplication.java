f9b7fcfcf3cc3ecd51c1872f694a434231409c84
ayotaylor <ayo.taylor@gmail.com>
2024-09-04 18:56:51 -0500

    created skeletons of user+gameservice with apigateway+eurekaserver

---
+++ gameservice/src/main/java/com/patchnotes/gameservice/GameserviceApplication.java
@@ -0,0 +1,15 @@
+package com.patchnotes.gameservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GameserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameserviceApplication.class, args);
+	}
+
+}
